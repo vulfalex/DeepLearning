0fc4343ecd5757986d6b2887870eb1b498344dda
vulfalex <53111070+vulfalex@users.noreply.github.com>
2020-04-04 08:27:09 +0300

    Update server.py

--- carInfra/app/server.py
+++ carInfra/app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 #export_file_url = 'https://drive.google.com/open?id=1WVhUOkD5O-i--ISqHfsY9jXlctQKImn_'
 #export_file_url = 'https://drive.google.com/file/d/1WVhUOkD5O-i--ISqHfsY9jXlctQKImn_/view?usp=sharing'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1WVhUOkD5O-i--ISqHfsY9jXlctQKImn_'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1WVhUOkD5O-i--ISqHfsY9jXlctQKImn_'
 export_file_name = 'export.pkl'
 
 classes = ['ford', 'mazda', 'skoda']
